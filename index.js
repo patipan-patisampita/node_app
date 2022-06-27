fb8d7e5c4842746d7f3ee87b1944bc88f7170e14
patipan-patisampita <patipan.patisampita@gmail.com>
2022-06-28 02:11:38 +0700

    first init

---
+++ index.js
@@ -0,0 +1,13 @@
+// const express = require('express');
+import express from "express";
+
+const app = express();
+const port = 9000;
+
+app.use("/", (req, res) => {
+  res.json({ message: "Hello From Express App" });
+});
+
+app.listen(port, () => {
+  console.log(`Starting Server at http://localhost:${port}`);
+});
