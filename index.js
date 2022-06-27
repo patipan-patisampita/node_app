f5cbdf1d67e34f70f5e513bc717aa2599bea9455
patipan-patisampita <patipan.patisampita@gmail.com>
2022-06-28 02:46:48 +0700

    update

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-const express = require('express');
+// Add Express
+const express = require("express");
 // import express from "express";
 
-const app = express();
 const port = 9000;
 
-app.use("/", (req, res) => {
+// Initialize Express
+const app = express();
+
+// Create GET request
+app.get("/", (req, res) => {
+  // res.send("Express on Vercel");
   res.json({ message: "Hello From Express App" });
 });
 
-app.listen(port, () => {
+// Initialize server
+app.listen(5000, () => {
   console.log(`Starting Server at http://localhost:${port}`);
 });
